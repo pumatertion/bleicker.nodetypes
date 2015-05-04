a001f8164221947e1bca67592dde461fd40a52a4
Carsten Bleicker <carsten@bleicker.de>
2015-05-04 19:50:05 +0200

    Introduce column::offset and column::colspan

--- src/Column.php
+++ src/Column.php
@@ -11,4 +11,45 @@ use Bleicker\Nodes\AbstractContentNode;
  */
 class Column extends AbstractContentNode {
 
+	/**
+	 * @var integer
+	 */
+	protected $colspan;
+
+	/**
+	 * @var integer
+	 */
+	protected $offset;
+
+	/**
+	 * @param int $colspan
+	 * @return $this
+	 */
+	public function setColspan($colspan = NULL) {
+		$this->colspan = $colspan;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getColspan() {
+		return $this->colspan;
+	}
+
+	/**
+	 * @param int $offset
+	 * @return $this
+	 */
+	public function setOffset($offset = NULL) {
+		$this->offset = $offset;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getOffset() {
+		return $this->offset;
+	}
 }
