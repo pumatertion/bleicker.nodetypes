3124e47e3d0983389e2576cbd84d898e92cb236d
Carsten Bleicker <carsten@bleicker.de>
2015-05-09 21:18:52 +0200

    Register entityManager with add() instead of register()

--- tests/Functional/Configuration/Persistence.php
+++ tests/Functional/Configuration/Persistence.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Tools\Setup;
 Registry::set('doctrine.schema.paths.nodes', __DIR__ . "/../../../vendor/bleicker/nodes/src/Schema/Persistence");
 Registry::set('doctrine.schema.paths.nodetypes', __DIR__ . "/../../../src/Schema/Persistence");
 
-ObjectManager::register(EntityManagerInterface::class, function () {
+ObjectManager::add(EntityManagerInterface::class, function () {
 	return EntityManager::create(
 		Registry::get('DbConnection'),
 		Setup::createYAMLMetadataConfiguration(Registry::get('doctrine.schema.paths'), TRUE)
